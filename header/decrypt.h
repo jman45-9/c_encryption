0fb93c3dffec07484869fd819d00e8d6873ce940
Jeremy Cope <jcope0787@protonmail.com>
2023-12-27 17:15:29 -0700

    added files to setup decryption

---
+++ header/decrypt.h
@@ -0,0 +1,8 @@
+#ifndef C_ENCRYPTION_DECRYPTH
+#define C_ENCRYPTION_DECRYPTH
+
+#include <stdio.h>
+
+void decryptTarget(char *targetName, char *cypherName, char *outputFile);
+
+#endif //C_ENCRYPTION_DECRYPTH
---
+++ src/decrypt.c
@@ -0,0 +1,12 @@
+/*
+ * Filename: decrypt.c
+ * Purpose: functions to decrypt target file
+ * Author: Jeremy Cope
+ * Coptright: 2023 Jeremy Cope
+*/
+
+#include "../header/decrypt.h"
+
+void decryptTarget(char *targetName, char *cypherName, char *outputFile)
+{
+}
